7ad25b1983ba2bec3c96fd253306d954457a1dfb
langedev <public@daltonlange.com>
2023-11-20 02:22:55 -0800

    Remove extra rofi data

--- programs/rofi/default.nix
+++ programs/rofi/default.nix
@@ -3,28 +3,4 @@
 {
   programs.rofi.enable = true;
   programs.rofi.package = pkgs.rofi-wayland;
-  programs.wofi.settings = {
-    show = "dmenu"; # Default to dmenu
-    prompt = "";
-    hide_scroll = true;
-    insensitive = true;
-    location = "bottom_right";
-    dynamic_lines = true;
-    yoffset = -100;
-    xoffset = -40;
-    height = "50%";
-    width = "50%";
-  };
-  programs.wofi.style = ''
-    window {
-      background: rgba(0, 0, 0, 255);
-      font-size: 4rem;
-    }
-    #entry, #input {
-      margin: 2px;
-      background: #FFFFFF;
-      border-width: 2px;
-      border-color: #000000;
-    }
-  '';
 }
