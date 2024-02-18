4a5e71f4ade21cdfb005524ecce5319f2fa31889
langedev <public@daltonlange.com>
2024-02-17 18:06:27 -0800

    Faster boot timeout, and updated

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -2,7 +2,9 @@
 
 {
   # Use the systemd-boot EFI boot loader.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.loader.systemd-boot.enable = true;
+  boot.loader.timeout = 1;
   boot.loader.efi.canTouchEfiVariables = true;
 
   time.timeZone = "America/Los_Angeles";
