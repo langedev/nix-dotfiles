29928610bfdb4f02edbe81e438be859b19667694
Julia Lange <public@julialange.org>
2024-04-18 15:41:58 -0700

    Nvidia, add opencl icd and runtime

--- nixosModules/services/graphics/nvidia/default.nix
+++ nixosModules/services/graphics/nvidia/default.nix
@@ -12,6 +12,10 @@
       enable = true;
       driSupport = true;
       driSupport32Bit = true;
+      extraPackages = with pkgs; [
+        rocm-opencl-icd
+        rocm-opencl-runtime
+      ];
     };
 
     services.xserver.videoDrivers = ["nvidia"];
