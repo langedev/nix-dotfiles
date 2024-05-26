d5d7b5ce7cb413f9c17418bac15c649916ee4440
Julia Lange <public@julialange.org>
2024-05-26 16:55:15 -0700

    Bump flakes, use hyprland?submodules=1

--- flake.nix
+++ flake.nix
@@ -1,14 +1,10 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "nixpkgs/nixos-23.11";
 
-    hyprland.url = "github:hyprwm/Hyprland";
-    hyprland.inputs.nixpkgs.follows = "nixpkgs";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
     sops-nix.url = "github:Mic92/sops-nix";
-    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
     aagl.url = "github:ezKEa/aagl-gtk-on-nix";
-    aagl.inputs.nixpkgs.follows = "nixpkgs-stable";
   };
 
   outputs = { self, nixpkgs, ... }@inputs:
