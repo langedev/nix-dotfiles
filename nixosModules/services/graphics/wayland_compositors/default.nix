d524f7c0683ccb24de05baf40b20e8beb6779c29
Julia Lange <public@julialange.org>
2024-07-14 04:04:58 -0700

    Hypr, fix cachix support

--- nixosModules/services/graphics/wayland_compositors/default.nix
+++ nixosModules/services/graphics/wayland_compositors/default.nix
@@ -10,12 +10,12 @@
     inputs.hyprland.nixosModules.default
   ];
 
-  config = lib.mkIf config.hyprland.enable {
-    programs.hyprland.enable = true;
+  config = {
     nix.settings = {
       substituters = [ "https://hyprland.cachix.org" ];
       trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
       experimental-features = [ "nix-command" "flakes" ];
     };
+    programs.hyprland.enable = lib.mkIf config.hyprland.enable true;
   };
 }
