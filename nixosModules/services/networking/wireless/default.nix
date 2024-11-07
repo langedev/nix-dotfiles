7de3d0c3c76b7ba2cc420118d22c721f75e5e516
Julia Lange <public@julialange.org>
2024-11-07 14:59:12 -0800

    Wireless, host specified networks

--- nixosModules/services/networking/wireless/default.nix
+++ nixosModules/services/networking/wireless/default.nix
@@ -2,7 +2,11 @@
 
 {
   options = {
-    wireless.enable = lib.mkEnableOption "Enables wireless";
+    wireless = {
+      enable = lib.mkEnableOption "Enables wireless";
+      networks = lib.mkOption { default = {}; };
+      secretsFile = lib.mkOption {};
+    };
   };
 
   config = lib.mkIf config.wireless.enable {
@@ -12,20 +16,14 @@
         enable = true;
         group = "network";
       };
-      networks = {
-        "foofoo2" = {
-          pskRaw = "1269f8db0aec9d9c30bff6de9951f0ab2d18d7cea4dacd8bd253ebc237e73e2d";
-          priority = 1;
-        };
-        "OrbiWanKenobe" = {
-          pskRaw = "0128447a146d359c1224e558b0f4e3fc0a53b0b4a178334075b50f8d07e179d2";
-          priority = 1;
-        };
-        "Homura" = {
-          pskRaw = "dd77456e1df3f2c17bccc917a0e609684fa6d2ccb6a2e0fb122d50d243e0c87e";
-          priority = 0;
-        };
-      };
+
+      secretsFile = config.wireless.secretsFile;
+
+      networks = let
+        ensurePasswords = networks: lib.attrsets.mapAttrs (
+          name: value: { pskRaw = "ext:psk_" + name; } // value
+        ) networks;
+      in ensurePasswords config.wireless.networks;
     };
 
     networking.enableIPv6 = false;
