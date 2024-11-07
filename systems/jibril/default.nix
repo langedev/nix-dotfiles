a7425d21e3e300e7a4e021c5a3c391ec1b30da22
Julia Lange <public@julialange.org>
2024-11-07 15:00:00 -0800

    Jibril, update secrets, wireless and shell

--- systems/jibril/default.nix
+++ systems/jibril/default.nix
@@ -8,10 +8,23 @@
   nixpkgs.config.allowUnfree = true;
   system.stateVersion = "23.05";
 
+  sops-nix = {
+    enable = true;
+    keyFile = "/etc/sops/age/keys.txt";
+    sopsFile = ./secrets.yaml;
+    secrets = {
+      wireless = {};
+    };
+  };
+
   pipewire.enable = true;
   polkit.enable = true;
   bluetooth.enable = true;
-  wireless.enable = true;
+  wireless = {
+    enable = true;
+    networks = builtins.fromTOML (builtins.readFile ./networks.toml);
+    secretsFile = config.sops.secrets.wireless.path;
+  };
 
   user.name = "pan";
   user.timezone = "America/Los_Angeles";
@@ -20,6 +33,7 @@
   hyprland.enable = true;
 
   librewolf.enable = true;
-  xonsh.enable = true;
+  fish.enable = true;
+  fish.setDefault = true;
   syncthing.enable = true;
 }
