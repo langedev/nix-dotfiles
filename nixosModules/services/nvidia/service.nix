863891641f1e7eba165cc33db89c2ebd5920334c
Julia Lange <public@julialange.org>
2025-02-23 14:40:00 -0800

    Nvidia, add option to use open drivers

--- nixosModules/services/nvidia/service.nix
+++ nixosModules/services/nvidia/service.nix
@@ -4,20 +4,18 @@
   options.nvidia = {
     enable = lib.mkEnableOption
       "Enables nvidia with proprietary drivers";
+    open = lib.mkEnableOption "Use open nvidia option for newer cards";
   };
 
   config = lib.mkIf config.nvidia.enable {
     hardware.graphics = {
       enable = true;
-      extraPackages = with pkgs; [
-        rocm-opencl-icd
-        rocm-opencl-runtime
-      ];
     };
 
     services.xserver.videoDrivers = ["nvidia"];
 
     hardware.nvidia = {
+      open = if config.nvidia.open then true else false;
       modesetting.enable = true;
       package = config.boot.kernelPackages.nvidiaPackages.beta;
     };
