eed568e289a5a001d5f1b2248daef6e1af9fe658
Julia Lange <public@julialange.org>
2025-03-17 16:18:17 -0700

    Juri, add sops and pds invocation

--- hosts/juri/host.nix
+++ hosts/juri/host.nix
@@ -8,6 +8,22 @@
   system.timezone = "America/Los_Angeles";
   system.users.bigWheels = [ "pan" ];
 
+  sops-nix = {
+    enable = true;
+    keyFile = "/etc/sops/age/keys.txt";
+    sopsFile = ./secrets.yaml;
+    secrets = {
+      pdsEnv = {};
+    };
+  };
+
+  pds = {
+    enable = true;
+    hostname = "juri.woach.me";
+    adminEmail = "admin@woach.me";
+    environmentFile = config.sops.secrets.pdsEnv.path;
+  };
+
   shell.enabledShells = [ "fish" ];
   shell.defaultShell = "fish";
 
