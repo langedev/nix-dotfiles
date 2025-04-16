90f412816424edf5e5ee3aec8aca518eb76a0c2d
Julia Lange <public@julialange.org>
2025-04-16 11:52:16 -0700

    Fish, fix pull/push prompt and change characters
    The prompt has been broken for a while now on pull/push, this is because
    it was using unicode characters that were larger in the terminal,
    causing wraps that would reprint lines. I changed the characters for
    push/pull because of it.
    
    I also removed the stash character, and changed the dirty and staged
    characters to match the new characters

--- hmModules/apps/fish/functions/git_prompt.fish
+++ hmModules/apps/fish/functions/git_prompt.fish
@@ -43,22 +43,22 @@ if git_is_repo
     if test (command git ls-files --others --exclude-standard | wc -w 2> /dev/null) -gt 0
         set git_meta "$git_meta?"
     end
-    if test (command git rev-list --walk-reflogs --count refs/stash 2> /dev/null)
-        set git_meta "$git_meta\$"
-    end
+    # if test (command git rev-list --walk-reflogs --count refs/stash 2> /dev/null)
+    #     set git_meta "$git_meta\$"
+    # end
     if git_is_touched
-        git_is_dirty && set git_meta "$git_meta⨯"
-        git_is_staged && set git_meta "$git_meta●"
+        git_is_dirty && set git_meta (string join "$git_meta" "x")
+        git_is_staged && set git_meta (string join "$git_meta" "o")
     end
     set -l commit_count (command git rev-list --count --left-right (git remote)/(git_branch_name)"...HEAD" 2> /dev/null)
     if test $commit_count
         set -l behind (echo $commit_count | cut -f 1)
         set -l ahead (echo $commit_count | cut -f 2)
         if test $behind -gt 0
-            set git_meta "$git_meta🠋"
+            set git_meta "$git_meta«"
         end
         if test $ahead -gt 0
-            set git_meta "$git_meta🠉"
+            set git_meta "$git_meta»"
         end
     end
     if test $git_meta
